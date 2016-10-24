2d3fc264e9eda83f5fcd521ac14b51349b9b45c2
Daniel <daniel-yim@live.com>
2016-10-24 17:59:42 +0800

    Update test.js
    添加启动服务

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
+var http = require('http');
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080, 
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+console.log("Listen on :" + ip + ":" + port);
 console.log("Hello openshift");
+
+var server = http.createServer(function (req, res) {
+  res.end("Hello openshift");
+});
+
+http.listen(port, ip);
